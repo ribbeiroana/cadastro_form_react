c3a81ecea9cea61c819756483c3f8f018a3adf5e
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2024-10-30 11:46:33 -0300

    register style

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import RegisterForm from './components/RegisterForm/registerForm.tsx';
+function App() {
+  return (
+    <div>
+      <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RegisterForm/registerForm.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './registerForm.css';
+import { FaUser, FaLock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+const LoginForm = () => {
+  return (
+      <div className='wrapper'>
+        <form action="">
+          <h1>REGISTER</h1>
+          <div className="input-box">
+            <input type="email" placeholder='Email' required/>
+            <MdEmail className='icon' />
+          </div>
+          <div className="input-box">
+            <input type="text" placeholder='Username' required/>
+            <FaUser className='icon' />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder='Password' required/>
+            <FaLock className='icon'/>
+          </div>
+
+          <button type="submit">REGISTER</button>
+        </form>
+
+      </div>
+  )
+}
+
+export default LoginForm;
